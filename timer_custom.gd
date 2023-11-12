50fd2105bf9ed21676c7fca4a46a4d5d2087b4da
Felipe Facundes <felipe.facundes@gmail.com>
2023-11-11 22:19:45 -0300

    Update timer_custom.gd
    create timer without node on scene

--- timer_custom.gd
+++ timer_custom.gd
@@ -14,3 +14,12 @@ func run_a_any_function():
 if not self.has_node("Timer"):
     self.add_child(timer)
     
+
+# ----------------------------------------------------------------------------------
+# Example de function to singleton Global
+func create_timer(the_timer_var: Timer, run_a_any_function, _delay: float):
+	add_child(the_timer_var)  # adds the Timer as a child node
+	the_timer_var.wait_time = _delay  # sets the wait time to 1 second
+	the_timer_var.one_shot = true  # sets the Timer to trigger only once
+	the_timer_var.connect("timeout", run_a_any_function)  # connects the timeout signal to 'run a any method'
+	the_timer_var.start()  # starts the Timer
